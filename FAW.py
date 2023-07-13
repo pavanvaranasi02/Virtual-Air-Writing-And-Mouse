3ebd45f4ca76998499ee236423e2baf845bdf97f
nagasaipavan1 <104422334+nagasaipavan1@users.noreply.github.com>
2023-07-13 21:29:33 +0530

    Final version

--- FAW.py
+++ FAW.py
@@ -1,3 +1,4 @@
+import signal
 import time
 import cv2
 import mediapipe as mp
@@ -63,18 +64,23 @@ def mask_frame(img):
 
     return masked
 
+def run_another_file():
+    cap.release()
+    cv2.destroyAllWindows()
+    # Specify the path to the Python file you want to run
+    another_file_path = "virtual_mouse_hands.py"
+    os.system("python " + another_file_path)
+    os.kill(os.getpid(), signal.SIGTERM)
+
 
 # importing cnn model
 cnn_model = keras.models.load_model('emnist_mrf_cnn_model.h5')
-
-# Import rfc model
-# import joblib
-# model = joblib.load('emnist_rfc_model.sav')
+cnn_digital_model = keras.models.load_model('emnist_digit_cnn_model.h5')
 
 # Map the index to a character using a dictionary 
 mapp = {}
 
-with open('D:\\MiniProject\\dictfile.txt', 'r') as f:
+with open('dictfile.txt', 'r') as f:
     lines = f.readlines()
     for line in lines:
         line = line.split(' ')
@@ -82,7 +88,6 @@ with open('D:\\MiniProject\\dictfile.txt', 'r') as f:
 
 
 # Create a video capture object
-# cap = cv2.VideoCapture(0)
 cap = cv2.VideoCapture(0)
 
 cap.set(3, 1080)
@@ -90,7 +95,7 @@ cap.set(4, 720)
 
 # Create a mediapipe hands object
 mpHands = mp.solutions.hands
-hands = mpHands.Hands(max_num_hands = 1, min_detection_confidence = 0.8)
+hands = mpHands.Hands(max_num_hands = 1)
 
 # Create a mediapipe drawing object
 mpDraw = mp.solutions.drawing_utils
@@ -116,10 +121,13 @@ speak_x, speak_y = 540, 75 # top left corner of speak button
 save_x, save_y = 660, 75 # top right corner of save button
 color_x, color_y = 790, 75 # top right corner of save button
 mouse_x, mouse_y = 920, 75 # top right corner of mouse button
+ad_x, ad_y = 1050, 75 # top right corner of mouse button
 button_w, button_h = 130, 75 # width and height of buttons
 
 # Define a string variable to display on the screen
 text = ""
+
+is_digit = False
 
 # continue the sentence when we come after certain duration.
 option = pyautogui.confirm('Select an option', buttons =['1>> Start Fresh', '2>> Continue from the previous Save of txt file', '3>> Continue from the previous Save of docx file'])
@@ -159,6 +167,10 @@ while cap.isOpened():
 
     # Process the image with the hands object
     results = hands.process(imgRGB)
+    
+    # Show mode which it is running in.
+    mode = 'Alpha' if(not is_digit) else 'Digit'
+    cv2.putText(img, mode, (850, 200), font, fontScale, (255, 200, 100), thickness, cv2.LINE_AA)
 
     # Check if there are any hand landmarks detected
     if results.multi_hand_landmarks:
@@ -230,21 +242,32 @@ while cap.isOpened():
                     masked = mask_frame(canvas)
 
                     # Use the model to predict the character 
-                    prediction = cnn_model.predict(masked)
+                    if not is_digit:
+                        prediction = cnn_model.predict(masked)
+                        
+                        print(prediction)
+                        
+                        # Get the index of the highest probability 
+                        index = np.argmax(prediction)
+                        
+                        # ascii value is stored from the index returned by cnn model.
+                        asci_value = mapp[index+1]
 
-                    print(prediction)
-
-                    # Get the index of the highest probability 
-                    index = np.argmax(prediction)
+                        # using chr and ascii value we can get that character drawn in the frame.
+                        char = chr(asci_value)
+                        
+                        # Append the character to text variable 
+                        text += char
                     
-                    # ascii value is stored from the index returned by cnn model.
-                    asci_value = mapp[index+1]
+                    else:
+                        prediction = cnn_digital_model.predict(masked)
 
-                    # using chr and ascii value we can get that character drawn in the frame.
-                    char = chr(asci_value)
+                        print(prediction)
+                        
+                        # Get the index of the highest probability 
+                        index = np.argmax(prediction)
 
-                    # Append the character to text variable 
-                    text += char
+                        text += str(index)
 
                     # so all the circles drawn till now are erased.
                     circles = []
@@ -273,13 +296,13 @@ while cap.isOpened():
                     option = pyautogui.confirm('Select an option', buttons =['1>> Save in txt file', '2>> Save in docx file'])
 
                     if option == '1>> Save in txt file':
-                        fileName = "D:\\MiniProject\\newfile.txt"
+                        fileName = "newfile.txt"
                         with open(fileName, 'w') as f:
                             f.write(text)
                         pyautogui.alert(f'Stored in CWD with filename as {fileName}')
                         
                     else:
-                        filename = 'D:\\MiniProject\\newfile.docx'
+                        filename = 'newfile.docx'
                         with open(filename, 'w') as f:
                             f.write(text)
                         pyautogui.alert(f'Stored in CWD with filename as {filename}')
@@ -291,7 +314,7 @@ while cap.isOpened():
                 elif color_x <= cx <= color_x + button_w and color_y <= cy <= color_y + button_h:
                     print("Change Color button pressed")
 
-                    option = pyautogui.confirm('Select an option', buttons =['Blue', 'Green', 'Red', 'White', 'Black', 'Pink'])
+                    option = pyautogui.confirm('Select an option', buttons =['Blue', 'Green', 'Red', 'White', 'Black', 'Pink', 'Brown', 'Orange', 'Yellow'])
 
                     if option == 'Blue':
                         color = (255, 0, 0)
@@ -311,6 +334,15 @@ while cap.isOpened():
                     elif option == 'Pink':
                         color = (255, 0, 255)
                         
+                    elif option == 'Brown':
+                        color = (0, 75, 150)
+                        
+                    elif option == 'Orange':
+                        color = (0, 165, 255)
+                        
+                    elif option == 'Yellow':
+                        color = (0, 255, 255)
+                        
                     print('Changing color of the pen.... ')
 
                     time.sleep(1)
@@ -323,21 +355,27 @@ while cap.isOpened():
                     if option == '1>> Speak':
                         speak(text)
                     elif option == '2>> Save in txt file':
-                        fileName = "D:\\MiniProject\\newfile.txt"
+                        fileName = "newfile.txt"
                         with open(fileName, 'w') as f:
                             f.write(text)
                         pyautogui.alert(f'Stored in CWD with filename as {fileName}')
                     else:
-                        filename = 'D:\\MiniProject\\newfile.docx'
+                        filename = 'newfile.docx'
                         with open(filename, 'w') as f:
                             f.write(text)
                         pyautogui.alert(f'Stored in CWD with filename as {filename}')
+                        
+                        
+                    print('Opening Virtual Mouse ...')
 
-                    chosen_mouse_operations = True  
-                    cap.release()         
-                    cv2.destroyAllWindows()
-                    print('Cap released')
-                    break
+                    run_another_file()
+                
+                elif ad_x <= cx <= ad_x + button_w and ad_y <= cy <= ad_y + button_h:
+                    print("Alpha digit button pressed")
+
+                    is_digit = not is_digit
+                    
+                    time.sleep(1)
 
                     
     # Loop through the circles list and draw them on the image with white color  
@@ -346,12 +384,12 @@ while cap.isOpened():
         cv2.circle(img,circle[:2],circle[2],color,-1)
         cv2.circle(canvas,circle[:2],circle[2],(255, 255, 255),-1)
 
-        # Check if any circle is clicked by the index finger tip 
-        if abs(x1 - circle[0]) < circle[2] and abs(y1 - circle[1]) < circle[2]:
-            print(f"Circle {i} clicked")
+        # # Check if any circle is clicked by the index finger tip 
+        # if abs(x1 - circle[0]) < circle[2] and abs(y1 - circle[1]) < circle[2]:
+        #     print(f"Circle {i} clicked")
             
-            # Keep the circle information in the list unchanged 
-            circles[i] = circle
+        #     # Keep the circle information in the list unchanged 
+        #     circles[i] = circle
 
 
     # Draw three buttons on the image with white color and black text 
@@ -379,6 +417,9 @@ while cap.isOpened():
     
     cv2.rectangle(img,(mouse_x,mouse_y),(mouse_x+button_w,mouse_y+button_h),(255,255,255),-1) # mouse button 
     cv2.putText(img,"Mouse",(mouse_x+25,mouse_y+50),font,fontScale,(0,0,0),thickness,cv2.LINE_AA) # mouse text 
+    
+    cv2.rectangle(img,(ad_x,ad_y),(ad_x+button_w,ad_y+button_h),(255,255,255),-1) # alpha_digit button 
+    cv2.putText(img,"Alpha/Digit",(ad_x+25,ad_y+50),font,fontScale,(0,0,0),thickness,cv2.LINE_AA) 
 
     # Display the text variable on the top of the screen with white color and black background 
     cv2.rectangle(img,(0 ,0),(w ,100),(0 ,0 ,0),-1) # black background 
@@ -405,17 +446,12 @@ if(not chosen_mouse_operations):
     if option == '1>> Speak':
         speak(text)
     elif option == '2>> Save in txt file':
-        fileName = "D:\\MiniProject\\newfile.txt"
+        fileName = "newfile.txt"
         with open(fileName, 'w') as f:
             f.write(text)
         pyautogui.alert(f'Stored in CWD with filename as {fileName}')
     elif option == '3>> Save in docx file':
-        filename = 'D:\\MiniProject\\newfile.docx'
+        filename = 'newfile.docx'
         with open(filename, 'w') as f:
             f.write(text)
         pyautogui.alert(f'Stored in CWD with filename as {filename}')
-    
-else:
-    import subprocess
-    path = "virtual_mouse_hands.py"
-    process = subprocess.Popen(["python", path])
--- virtual_mouse_hands.py
+++ virtual_mouse_hands.py
@@ -1,3 +1,4 @@
+import signal
 import cv2
 import numpy as np
 import time
@@ -7,9 +8,9 @@ import math
 import pyautogui
 
 ##########################
-wCam, hCam = 640, 480
-frameR = 100  # Frame Reduction
-smoothening = 6
+wCam, hCam = 1080, 720
+frameR = 200  # Frame Reduction
+smoothening = 8
 #########################
 
 cap = cv2.VideoCapture(0)
@@ -27,84 +28,154 @@ detector = htm.handDetector(maxHands=1)
 wScr, hScr = pyautogui.size()
 #print(wScr, hScr)
 
+# Define the font and size of the buttons text
+font = cv2.FONT_HERSHEY_SIMPLEX
+fontScale = 1
+
+# Define button positions and sizes
+run_button_x = 200
+run_button_y = 70
+run_button_width = 350
+run_button_height = 100
+
+stop_button_x = 600
+stop_button_y = 70
+stop_button_width = 150
+stop_button_height = 100
+
+# Define colors
+button_color = (255, 0, 0)  # Blue
+text_color = (255, 255, 255)  # White
+
+def run_another_file():
+    cap.release()
+    cv2.destroyAllWindows()
+    # Specify the path to the Python file you want to run
+    another_file_path = "FAW.py"
+    os.system("python " + another_file_path)
+    os.kill(os.getpid(), signal.SIGTERM)
+
 while True:
-    # 1. Find hand Landmarks
+    # Find hand Landmarks
     success, img = cap.read()
+    
+    # Flip the axis horizontally
+    img = cv2.flip(img, 1)
+    
     img = detector.findHands(img)
     lmList, bbox = detector.findPosition(img)
-    # 2. Get the tip of the index and middle fingers
+    
+    cv2.rectangle(img, (frameR, frameR), (wCam - frameR, hCam - frameR), (255, 0, 255), 2)
+    
+    # Check if the run button is clicked
     if len(lmList) != 0:
         x1, y1 = lmList[8][1:]
         x2, y2 = lmList[12][1:]
-        #print(x1, y1, x2, y2)
+        cx, cy = 0, 0
+        # Check if the index and middle finger are close enough (within 40 pixels)
+        if abs(x1 - x2) < 40 and abs(y1 - y2) < 40:
+            # Calculate the center of index and middle coordinates 
+            cx = (x1 + x2) // 2 
+            cy = (y1 + y2) // 2 
+        if run_button_x < cx < run_button_x + run_button_width and run_button_y < cy < run_button_y + run_button_height:
+            print('Opening Finger Air Writing ...')
+            run_another_file()
 
-        # 3. Check which fingers are up
+    # Check if the stop button is clicked
+    if len(lmList) != 0:
+        x1, y1 = lmList[8][1:]
+        x2, y2 = lmList[12][1:]
+        cx, cy = 0, 0
+        # Check if the index and middle finger are close enough (within 40 pixels)
+        if abs(x1 - x2) < 40 and abs(y1 - y2) < 40:
+            # Calculate the center of index and middle coordinates 
+            cx = (x1 + x2) // 2 
+            cy = (y1 + y2) // 2 
+        length, img, lineInfo = detector.findDistance(8, 12, img)
+        if stop_button_x < cx < stop_button_x + stop_button_width and stop_button_y < cy < stop_button_y + stop_button_height:
+            print('Closing Virtual Mouse ...')
+            break
+    
+    # Get the tip of the index and middle fingers
+    if len(lmList) != 0:
+        x0, y0 = lmList[4][1:]
+        x1, y1 = lmList[8][1:]
+        x2, y2 = lmList[12][1:]
+        x3, y3 = lmList[16][1:]
+        x4, y4 = lmList[20][1:]
+
+        # Check which fingers are up
         fingers = detector.fingersUp()
-        # print(fingers)
-        cv2.rectangle(img, (frameR, frameR), (wCam - frameR, hCam - frameR), (255, 0, 255), 2)
 
-
-        # 12. if thumb and index finger are up volume operations.
-        # if fingers[0] == 1 and fingers[1] == 1:
-        #     lineLength, img, lineInfo = detector.findDistance(4, 8, img)
-        #     # Volume up
-        #     if lineLength < 40 and prevLength < lineLength:
-        #         pyautogui.press('volumeup')
-        #     # Volume down
-        #     elif lineLength < 40 and prevLength > lineLength:
-        #         pyautogui.press('volumedown')
-        #     prevLength = lineLength
-
-
-        # 4. Only Index Finger : Moving Mode
-        if fingers[1] == 1 and fingers[2] == 0:
-            # 5. Convert Coordinates
+       # if index, middle, and ring fingers are up then volume is increased
+        if fingers == [0, 1, 1, 1, 0]:
+            pyautogui.press('volumeup')
+            cv2.circle(img, (x1, y1), 15, (255, 255, 255), cv2.FILLED)
+            cv2.circle(img, (x2, y2), 15, (255, 255, 255), cv2.FILLED)
+            cv2.circle(img, (x3, y3), 15, (255, 255, 255), cv2.FILLED)
+            cv2.putText(img, "Volume Up", (500, 55), font, fontScale, (255, 0, 0), 3, cv2.LINE_AA)
+            
+        # if index, middle, ring, little fingers are up then volume is decreased
+        elif fingers == [0, 1, 1, 1, 1]:
+            pyautogui.press('volumedown')
+            cv2.circle(img, (x1, y1), 15, (255, 255, 255), cv2.FILLED)
+            cv2.circle(img, (x2, y2), 15, (255, 255, 255), cv2.FILLED)
+            cv2.circle(img, (x3, y3), 15, (255, 255, 255), cv2.FILLED)
+            cv2.circle(img, (x4, y4), 15, (255, 255, 255), cv2.FILLED)
+            cv2.putText(img, "Volume Down", (500, 55), font, fontScale, (255, 0, 0), 3, cv2.LINE_AA)
+        
+        # Only Index Finger: Moving Mode
+        elif fingers[0] == 0 and fingers[1] == 1 and fingers[2] == 0:
+            # Convert Coordinates
             x3 = np.interp(x1, (frameR, wCam - frameR), (0, wScr))
             y3 = np.interp(y1, (frameR, hCam - frameR), (0, hScr))
-            # 6. Smoothen Values
+            # Smoothen Values
             clocX = plocX + (x3 - plocX) / smoothening
             clocY = plocY + (y3 - plocY) / smoothening
 
-            # 7. Move Mouse
-            pyautogui.moveTo(wScr - clocX, clocY)
+            # Move Mouse
+            pyautogui.moveTo(clocX, clocY)
             cv2.circle(img, (x1, y1), 15, (255, 0, 255), cv2.FILLED)
+            cv2.putText(img, "Moving Mouse", (460, 55), font, fontScale, (255, 0, 0), 3, cv2.LINE_AA) # white text 
             plocX, plocY = clocX, clocY
 
-        # 8. Both Index and middle fingers are up : Clicking Mode
-        if fingers[1] == 1 and fingers[2] == 1:
-            # 9. Find distance between fingers
+        # Both Index and middle fingers are up: Clicking Mode
+        elif fingers[1] == 1 and fingers[2] == 1:
+            # Find distance between fingers
             length, img, lineInfo = detector.findDistance(8, 12, img)
-            #print(length)
-            # 10. Click mouse if distance short
+            # Click mouse if distance short
             if length < 40:
                 cv2.circle(img, (lineInfo[4], lineInfo[5]), 15, (0, 255, 0), cv2.FILLED)
+                cv2.putText(img, "Left Click", (500, 55), font, fontScale, (255, 0, 0), 3, cv2.LINE_AA) # white text 
                 pyautogui.click()
 
-        #  11. Only thumb right click
-        if fingers[0] == 1 and fingers[1] == 0:
-            pyautogui.rightClick()
+        # Thumb and index finger: Right Click
+        elif fingers[0] == 1 and fingers[1] == 1:
+            # Find distance between fingers
+            length, img, lineInfo = detector.findDistance(4, 8, img)
+            if length < 40:
+                cv2.putText(img, "Right Click", (500, 55), font, fontScale, (255, 0, 0), 3, cv2.LINE_AA) # white text 
+                pyautogui.rightClick()
 
-        
 
-        
+    # Draw run button
+    cv2.rectangle(img, (run_button_x, run_button_y), (run_button_x + run_button_width, run_button_y + run_button_height), button_color, cv2.FILLED)
+    cv2.putText(img, "Finger Air Writing", (run_button_x + 40, run_button_y + 55), font, fontScale, text_color, 2, cv2.LINE_AA)
 
-    # 13. Frame Rate
+    # Draw stop button
+    cv2.rectangle(img, (stop_button_x, stop_button_y), (stop_button_x + stop_button_width, stop_button_y + stop_button_height), button_color, cv2.FILLED)
+    cv2.putText(img, "Close", (stop_button_x + 40, stop_button_y + 55), font, fontScale, text_color, 2, cv2.LINE_AA)
+
+    # Frame Rate
     cTime = time.time()
     fps = 1 / (cTime - pTime)
     pTime = cTime
-    cv2.putText(img, str(int(fps)), (20,50), cv2.FONT_HERSHEY_PLAIN, 3, (255, 0, 0), 3)
+    cv2.putText(img, str(int(fps)), (20, 50), cv2.FONT_HERSHEY_PLAIN, 3, (255, 0, 0), 3)
 
-    # 14. display
+    # Display
     cv2.imshow('Virtual Mouse', img)
     if cv2.waitKey(1) == ord('q'):
         break
 
 cap.release()
 cv2.destroyAllWindows()
-   
-
-
-
-
-
-
